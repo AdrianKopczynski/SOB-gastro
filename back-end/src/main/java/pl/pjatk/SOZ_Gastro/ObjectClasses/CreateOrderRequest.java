78a4b4b68c3e180558f809b3b66d8f5df1832cc6
PatriStelmach <patrzy108@gmail.com>
2024-12-18 17:46:24 +0100

    fixing create order so now it works :--)

---
+++ back-end/src/main/java/pl/pjatk/SOZ_Gastro/ObjectClasses/CreateOrderRequest.java
@@ -0,0 +1,23 @@
+package pl.pjatk.SOZ_Gastro.ObjectClasses;
+
+public class CreateOrderRequest {
+    private Long[] mealID;
+    private Tabletop tabletop;
+
+    // Getters and Setters
+    public Long[] getMealID() {
+        return mealID;
+    }
+
+    public void setMealID(Long[] mealID) {
+        this.mealID = mealID;
+    }
+
+    public Tabletop getTabletop() {
+        return tabletop;
+    }
+
+    public void setTabletop(Tabletop tabletop) {
+        this.tabletop = tabletop;
+    }
+}
