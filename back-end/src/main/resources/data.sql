16097c5e0bf4781beb438e03f8440e82029eec03
mirqu <s27814@pjwstk.edu.pl>
2025-01-19 19:03:17 +0100

    add init Data +target directory removed from .gitignore

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -20,6 +20,18 @@ INSERT INTO tabletops (is_available, name) VALUES (true,'Lada');
 INSERT INTO tabletops (is_available, name) VALUES (true,'Stolik A1');
 INSERT INTO tabletops (is_available, name) VALUES (true,'Stolik A2');
 INSERT INTO tabletops (is_available, name) VALUES (true,'Stolik C1');
+INSERT INTO orders (closed_at, created_at, tabletop_id, user_id, comment) VALUES ('2023-10-01 13:00:00','2023-10-01 12:00:00',1,1,'pilne');
+INSERT INTO orders (created_at, tabletop_id, user_id) VALUES ('2023-10-01 12:01:00',2,1);
+INSERT INTO orders (created_at, tabletop_id, user_id) VALUES ('2023-10-01 12:07:00',3,1);
+INSERT INTO order_meal (meal_id, order_id) VALUES (1,1);
+INSERT INTO order_meal (meal_id, order_id) VALUES (7,1);
+INSERT INTO order_meal (meal_id, order_id) VALUES (9,1);
+INSERT INTO order_meal (meal_id, order_id) VALUES (4,2);
+INSERT INTO order_meal (meal_id, order_id) VALUES (5,2);
+INSERT INTO order_meal (meal_id, order_id) VALUES (8,2);
+INSERT INTO order_meal (meal_id, order_id) VALUES (7,2);
+INSERT INTO order_meal (meal_id, order_id, comment) VALUES (3,3, 'bez soli');
+INSERT INTO order_meal (meal_id, order_id) VALUES (2,3);
 
 
 
